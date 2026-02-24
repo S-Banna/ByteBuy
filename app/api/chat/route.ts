f6a83ee04e82fb20d2434fd5697a79372072c3c2
waelk06 <waelkaraki1234@gmail.com>
2026-02-24 11:16:09 +0200

    added the api key

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 export async function POST(request: Request) {
     const {msg} = await request.json();
