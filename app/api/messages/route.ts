d20a9f1a23e466c7d726cb00e54f8f4e4e128ddc
waelk06 <waelkaraki1234@gmail.com>
2026-04-01 18:24:53 +0300

    made get and post functions, waiting for db to be refactored

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,1 +1,17 @@
-// THIS WILL BE USED TO POST A MESSAGE
+import {Pool} from "pg";
+import {NextResponse} from "next/server";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+export async function GET(request: Request) {
+
+}
+
+export async function POST(request: Request) {
+    
+}
