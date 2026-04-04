32054132c01deb2393d26a80cdb86cf2f539bbe2
notOK18 <omarkh5lembo@gmail.com>
2026-04-04 22:03:23 +0300

    common link to db

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,1 +1,34 @@
-// THIS WILL ATTEMPT A SIGNUP
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { pool } from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Missing email or password" },
+        { status: 400 }
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email",
+      [email, hashedPassword]
+    );
+
+    const user = result.rows[0];
+
+    return NextResponse.json(user);
+
+  } catch (err: any) {
+    console.error(err);
+
+    return NextResponse.json(
+      { error: "User may already exist" },
+      { status: 500 }
+    );
+  }
+}
